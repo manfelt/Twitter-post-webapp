d2904c3317d565852aa0f222336162e6de8a160b
manfelt <35919896+manfelt@users.noreply.github.com>
2021-04-02 21:57:54 +0200

    Legger til funksjon 'splittStrengTilUnicode' som sørger for å splitte streng inn i en matrise og at unicode karakterer respekteres.

--- SendTilTwitter.php
+++ SendTilTwitter.php
@@ -3,49 +3,77 @@
 	// uavhengig kan stilles inn ettersom nødvendig.
 	// 'TwitterApiExchange.php' er en wrapper referert til av 'twitter developer' som 'Twitter-API-PHP': 
 	// https://developer.twitter.com/en/docs/twitter-api/tools-and-libraries
-	require_once('innstillinger.php');
+	require('innstillinger.php');
 	require_once('TwitterAPIExchange.php');
-	
-	// 'associative array', som 'mapper' nøkler fra innstillinger.php 
-	$innstillinger = array('consumer_key' => TWITTER_API_KEY, 'consumer_secret' => TWITTER_API_SECRET, 
-	'oauth_access_token' => TWITTER_ACCESS_TOKEN, 'oauth_access_token_secret' => TWITTER_ACCESS_TOKEN_SECRET );
 
-	// URL 
-	$url = 'https://api.twitter.com/1.1/statuses/update.json';
-
-	// bestemmer request-type
-	$requestMethode = 'POST';
 
 	// verdien til denne variabelen er innholdet som postes og sendes til twitter. 
     // Den skaffes direkte fra input feltet hos 'index.php'
-	$postTilTwitter = $_POST["postTweet"];
+	$postTilTwitter = $_POST['postTweet'];
 
-    $postTilTwitter_arr = str_split($postTilTwitter);
-
-    foreach ($postTilTwitter_arr as $twitterPost) {
-        if ( in_array($twitterpost, $forbudteKarakterer )) {
-            echo 'Tweet inneholder ugyldige karakterer';
-            break;
-        }
-    }
-
-    if (strlen($postTilTwitter) < $minimumTweetLengde) {
-        echo 'Tweet er for kort';
-    } elseif (strlen($postTilTwitter) > $maksimumTweetLengde) {
-        echo 'Tweet er for lang';
-    }
-
-
-    // 'associative array' som tar vare på twitter-post innholdet som skal sendes.
-	$apiData = array( 'status' => $postTilTwitter );
-
-	$twitter = new TwitterAPIExchange ( $innstillinger );
-	$twitter->buildOauth( $url, $requestMethode );
-	$twitter->setPostfields( $apiData );
-	$response = $twitter->performRequest( true, array( CURLOPT_SSL_VERIFYHOST => 0, CURLOPT_SSL_VERIFYPEER => 0) );
-
-	// <pre> - uformattert tekst
-	echo '<pre>';
-	print_r(json_decode( $response, true) );
 	
+	function evaluerTwitterPost ($postTilTwitter){
+		$tweetLengde = strlen($postTilTwitter);
+		$ugyldigeKarakterer = false;
+		$minimumTweetLengde = 20;
+		$maksimumTweetLengde = 140;
+		$forbudteKarakterer = array('Æ', 'Ø', 'Å', 'æ', 'ø', 'å');
+
+		// funksjon som splitter en streng til en array. Nødvendig ettersom str_splint() ikke tar hensyn til unicode.
+		function splittStrengTilUnicode($streng, $lengde = 1) {
+			$strengSplitt = preg_split('~~u', $streng, -1, PREG_SPLIT_NO_EMPTY);
+			if ($lengde > 1) {
+				$karakter = array_chunk($strengSplitt, $lengde);
+				
+				foreach ($karakter as $i => $karakter) {
+					$karakter[$i] = join('', (array) $karakter);
+				}
+				$strengSplitt = $karakter;
+			}
+			return $strengSplitt;
+		}
+
+		$postTilTwitterKarakterer = splittStrengTilUnicode($postTilTwitter);
+
+		foreach ($postTilTwitterKarakterer as $karakter) {
+			if ( in_array($karakter, $forbudteKarakterer )) {
+				$ugyldigeKarakterer = true;
+				break;
+			}
+		}
+		if ($tweetLengde < $minimumTweetLengde) {
+			echo 'Tweet er for kort ';
+		} elseif ($tweetLengde > $maksimumTweetLengde) {
+			echo 'Tweet er for lang ';
+		} elseif ($ugyldigeKarakterer) {
+			echo 'Tweet inneholder ugyldige karakterer ';
+		} else {
+
+				// URL - Ikke endre denne, om mindre man skal gjøre annet enn å opprette twitterposter.
+				$url = 'https://api.twitter.com/1.1/statuses/update.json';
+
+				// bestemmer request-type
+				$requestMethode = 'POST';
+
+				// 'associative array', som 'mapper' nøkler fra innstillinger.php 
+				$innstillinger = array('consumer_key' => TWITTER_API_KEY, 'consumer_secret' => TWITTER_API_SECRET, 
+				'oauth_access_token' => TWITTER_ACCESS_TOKEN, 'oauth_access_token_secret' => TWITTER_ACCESS_TOKEN_SECRET );
+			    // 'associative array' som tar vare på twitter-post innholdet som skal sendes.
+				$apiData = array( 'status' => $postTilTwitter );
+
+				$twitter = new TwitterAPIExchange ( $innstillinger );
+				$twitter->buildOauth( $url, $requestMethode );
+				$twitter->setPostfields( $apiData );
+				$response = $twitter->performRequest( true, array( CURLOPT_SSL_VERIFYHOST => 0, CURLOPT_SSL_VERIFYPEER => 0) );
+
+				// <pre> - uformattert tekst
+				echo '<pre>';
+				print_r(json_decode( $response, true) );
+			}
+		}
+
+	evaluerTwitterPost ($postTilTwitter);
+
+
+
 ?>
